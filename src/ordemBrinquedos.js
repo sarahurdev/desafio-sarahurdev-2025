330eb5ac111be6fdf9575a3908df7974d91b6134
ursarah <sarahurcezino@gmail.com>
2025-09-06 13:57:11 -0300

    feat: criando classe para ordenar os brinquedos

---
+++ src/ordemBrinquedos.js
@@ -0,0 +1,17 @@
+class OndemBrinquedo {
+
+    ordem(listaPessoa, brinqFav, especie) {
+        if (especie === "jabuti") {
+            return brinqFav.every((b) => listaPessoa.includes(b));
+        }
+
+        let i = 0;
+        for (let lista of listaPessoa) {
+            if (lista === brinqFav[i]) {
+                i++;
+            }
+            if (i === brinqFav.length) return true;
+        }
+        return false;
+    }
+}
