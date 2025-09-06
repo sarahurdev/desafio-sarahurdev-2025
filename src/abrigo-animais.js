817faeb10a7874aa914e51584c0fb7acac21be53
ursarah <sarahurcezino@gmail.com>
2025-09-06 12:32:33 -0300

    feat: mais adições

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,37 @@
+// Rex	cão	RATO, BOLA
+// Mimi	gato	BOLA, LASER
+// Fofo	gato	BOLA, RATO, LASER
+// Zero	gato	RATO, BOLA
+// Bola	cão	CAIXA, NOVELO
+// Bebe	cão	LASER, RATO, BOLA
+// Loco	jabuti	SKATE, RATO
+
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+
+    const animais = {
+      Rex: { especie: "cão", brinquedos: ["RATO", "BOLA"] },
+      Mimi: { especie: "gato", brinquedos: ["BOLA", "LASER"] },
+      Fofo: { especie: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
+      Zero: { especie: "gato", brinquedos: ["RATO", "BOLA"] },
+      Bola: { especie: "cão", brinquedos: ["CAIXA", "NOVELO"] },
+      Bebe: { especie: "cão", brinquedos: ["LASER", "RATO", "BOLA"] },
+      Loco: { especie: "jabuti", brinquedos: ["SKATE", "RATO"] },
+    };
+
+    const todosBrinquedosValidos = [];
+    for (let nomeAnimal in animais) {
+      for (let brinquedo of animais[nomeAnimal].brinquedos) {
+        if (!todosBrinquedosValidos.includes(brinquedo)) {
+          todosBrinquedosValidos.push(brinquedo);
+        }
+      }
+    }
+
+    const pessoa1 = brinquedosPessoa1.split(",")
+    const pessoa2 = brinquedosPessoa2.split(",")
+    const ordem = ordemAnimais.split(",")
   }
 }
 
